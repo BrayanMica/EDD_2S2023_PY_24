3ee262dde7142366573beac914b7a719949aa612
kali <kali@kali.kalu>
2023-08-13 19:24:32 -0600

    Creacion de archivos base

---
+++ EDD_PY1_Fase1/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    //Login de app
+    cout<<"************       EDD ProyectUp      *************"<<endl;
+    cout<<"Usuario: PM-201907343"<<endl;
+    cout<<"Password: pmPassword123"<<endl;
+
+    //Menu de ProyectUp
+    cout<<"************       EDD ProyectUp      ************"<<endl;
+    cout<<"************  Bienvenido PM-201907343 ************"<<endl;
+    cout<<"1. Cargar empleados"<<endl;
+    cout<<"2. Carga proyecto"<<endl;
+    cout<<"3. Crear tareas "<<endl;
+    cout<<"4. Asignar tareas"<<endl;
+
+    //Cargar Empleados
+    cout<<"************       EDD ProyectUp      ************"<<endl;
+    cout<<"************  Bienvenido PM-201907343 ************"<<endl;
+    cout<<"************    Menu Carga Empleados  ************"<<endl;    
+    cout<<"1. Cargar Manual"<<endl;
+    cout<<"2. Carga Masiva"<<endl;
+    cout<<"Elije una opcion: "<<endl;
+
+    //cargar manual empleados
+    cout<<"Nombre:"<<endl;
+    cout<<"Constraseña:"<<endl;
+    cout<<"Puestos disponibles:"<<endl;
+    cout<<"1. Developer Frontend"<<endl;
+    cout<<"2. Developer Backend"<<endl;
+    cout<<"3. Quality Assurance"<<endl;
+    cout<<"Elija una opcion:"<<endl;
+    cout<<"Agregado exitosamente, Id generado: BDEV-563"<<endl;   
+
+    //Carga masiva empleados
+    cout<<"Nombre de archivo: calificacion.csv"<<endl;
+    cout<<"Carga Exitosa..."<<endl;
+
+    //Crear Proyecto
+    cout<<"************       EDD ProyectUp      ************"<<endl;
+    cout<<"************  Bienvenido PM-201907343 ************"<<endl;
+    cout<<"************      Menu de proyecto    ************"<<endl;
+    cout<<"Nombre de proyecto: Sistema de invetarios shadai"<<endl;
+    cout<<"Tipo de Prioridad: A"<<endl;
+    cout<<"Creado exitosamente, PY-103"<<endl;
+
+    //Crear Tarea
+    cout<<"************       EDD ProyectUp      ************"<<endl;
+    cout<<"************  Bienvenido PM-201907343 ************"<<endl;
+    cout<<"************       Menu de Tareas     ************"<<endl;
+    cout<<"PY-100. Sistema de inventario Vestidos Julieta"<<endl;   
+    cout<<"PY-101. Tienda Online Restaurante Giovanni"<<endl;
+    cout<<"PY-102. Sistema de inventario EPA"<<endl;
+    cout<<"PY-103. Sistema de inventario Shadai"<<endl;
+    cout<<"Elije un proyecto: PY-102"<<endl;
+    cout<<"Nombre de la tarea: Generar consultas de area de Destornilladores"<<endl;
+
+    
+    cout<<""<<endl;
+return 0;
+}
